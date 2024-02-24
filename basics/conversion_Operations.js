01b6ece3c62ef4845faa154f620017e022609765
Vanshikavemula <142506903+Vanshikavemula@users.noreply.github.com>
2024-02-24 10:32:36 +0000

    Commit

--- basics/conversion_Operations.js
+++ basics/conversion_Operations.js
@@ -1,6 +1,6 @@
 let score = "Vanshika"
 
-console.log(typeof score);
+//console.log(typeof score);
 //console.log(typeof(score));
 
 let valueInNumber = Number(score)
@@ -27,11 +27,11 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
-//Operations
+//  **Operations**
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+//console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -44,17 +44,17 @@ let str1 = "hello"
 let str2 = " Vanshika"
 
 let str3 = str1 + str2
-// console.log(str3);
+//console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
+//console.log("1" + 2);
+//console.log(1 + "2");
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); //1
+// console.log(+"");   //0
 
 let num1, num2, num3
 
