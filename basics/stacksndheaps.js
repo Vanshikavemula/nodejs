b4b6818dc2e176320fba10600b1abb47b77c2de0
Vanshikavemula <142506903+Vanshikavemula@users.noreply.github.com>
2024-02-25 14:46:42 +0000

    stacks

---
+++ basics/stacksndheaps.js
@@ -0,0 +1,15 @@
+let myYoutubeName = "VanshikaVemula"
+let anothername = myYoutubeName
+anothername = "VemulaVanshika"
+console.log(anothername);
+console.log(myYoutubeName);
+
+let userOne = {
+    email: "vansh@gmail.com",
+    upi: "1234@axl"
+}
+
+let userTwo = userOne
+userTwo.email = "vanshi@gmail.com"
+ console.log(userTwo.email);
+ console.log(userOne.email);
