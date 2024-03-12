42a63f024bf41c91039de5ab84907d5927dacf45
Vemula Vanshika <vvemula3@gitam.in>
2024-03-12 22:55:52 +0530

    DOM

--- iterations/one.js
+++ iterations/one.js
@@ -14,8 +14,8 @@ for (let i = 0; i <= 10; i++) {
 for (let i = 1; i <= 5; i++) {
     console.log(`Outer loop value: ${i}`);
    for (let j = 1; j <= 5; j++) {
-    console.log(`Inner loop value ${j} and inner loop ${i}`);
-    console.log(i + '*' + j + ' = ' + i*j );
+    // console.log(`Inner loop value ${j} and inner loop ${i}`);
+    // console.log(i + '*' + j + ' = ' + i*j );
    }
     
 }
@@ -41,9 +41,9 @@ for (let index = 0; index < myArray.length; index++) {
 
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
-        // console.log(`Detected 5`);
+        console.log(`Detected 5`);
         continue
     }
-//    console.log(`Value of i is ${index}`);
+console.log(`Value of i is ${index}`);
     
 }
